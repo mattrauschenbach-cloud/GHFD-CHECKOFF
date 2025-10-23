89ad8231b021da385ffc29bcd3bb0566d4e26725
mattrauschenbach-cloud <mattrauschenbach@gmail.com>
2025-10-23 18:19:54 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
+import "./index.css"; // global styles (cards, header, etc.)
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 );
